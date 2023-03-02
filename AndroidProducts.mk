76f2952ccfaa6d9389c209abca16e4bd809673d9
Dhruv Tadvi <dhruvtadvi0x7@gmail.com>
2023-03-02 17:27:39 +0530

    finally a new tree by some modifications assume that as initial tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/arrow_KE6.mk
+
+COMMON_LUNCH_CHOICES := \
+    arrow_KE6-user \
+    arrow_KE6-userdebug \
+    arrow_KE6-eng
---
+++ arrow_KE6.mk
@@ -0,0 +1,24 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
+# Inherit from device makefile
+$(call inherit-product, device/tecno/KE6/device.mk)
+
+# Inherit some common ArrowOS stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := arrow_KE6
+PRODUCT_DEVICE := KE6
+PRODUCT_BRAND := TECNO
+PRODUCT_MODEL := TECNO_KE6
+PRODUCT_MANUFACTURER := texno
+
+PRODUCT_SYSTEM_MODEL := KE6
+PRODUCT_SYSTEM_NAME := KE6
+PRODUCT_SYSTEM_DEVICE := KE6
+
+# ArrowOS additions
+DEVICE_MAINTAINER := dhruvtadvi
+TARGET_BOOT_ANIMATION_RES := 720
